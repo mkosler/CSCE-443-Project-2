9be6a93b71174b41d60a8de4dea7e13f538f0ba7
Christopher A. Trask <chris.trask@gmail.com>
2013-10-22 18:08:56 -0500

    temp

--- main.lua
+++ main.lua
@@ -1,24 +1,49 @@
 ------------------------------------------------------------------------
 -- LIBRARIES -----------------------------------------------------------
 ------------------------------------------------------------------------
-
+ -- load GUI Lib( LoveFrames 0.9.6.4 Alpha )
+require('lib.LoveFrames')
+-- load Networking lib
+require('lib.LUBE.LUBE')
 local Gamestate = require 'lib.gamestate'
-
+local Title = require 'src.states.title'
 ------------------------------------------------------------------------
 -- GAMESTATES ----------------------------------------------------------
 ------------------------------------------------------------------------
 
 function love.load()
-  Gamestate.registerEvents()
+    Gamestate.registerEvents()
+    Gamestate.switch(Title)
+end
+
+function love.update(dt)
+    --update loop
+    loveframes.update(dt)
+end
+
+function love.draw()
+    loveframes.draw()
+end
+
+function love.mousepressed( x, y, button )
+    loveframes.mousepressed( x, y, button )
+end
+
+function love.mousereleased( x, y, button )
+    loveframes.mousereleased( x, y, button )
 end
 
 function love.keypressed(key, code)
-  -- Easy quitting for debug purposes
-  if key == 'escape' then
-    love.event.quit()
-  end
+    -- Easy quitting for debug purposes
+    loveframes.keypressed( key, code )
+end
+
+function love.keyreleased(key, code)
+    if key == 'escape' then
+        love.event.quit()
+    end
 end
 
 function love.quit()
-  print('Quitting...')
+    print('Quitting...')
 end
--- src/states/title.lua
+++ src/states/title.lua
@@ -1,6 +1,17 @@
 local Title = {}
 
 function Title:init()
+    self.frame = loveframes.Create("frame")
+    self.frame:ShowCloseButton(false)
+ 
+    -- method 1 using loveframes.Create
+    local_match = loveframes.Create("button", self.frame)
+    local_match:SetPos(5, 5)
+ 
+    -- method 2 using SetParent
+    local button2 = loveframes.Create("button")
+    button2:SetParent(self.frame)
+    button2:SetPos(5, 35)
 end
 
 function Title:enter(previous)
