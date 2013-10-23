739dd1ed3e05a2921c2dc220e15e69a910272058
Michael Kosler <marekkpie@gmail.com>
2013-10-22 23:18:57 -0500

    Changed name

--- conf.lua
+++ conf.lua
@@ -1,6 +1,6 @@
 --- The LOVE configuration function
 function love.conf(t)
-  t.title         = 'Strategy'
+  t.title         = 'SIGINT'
   t.authors       = 'Michael Kosler, Wally Tung, Stefan Levy, Christopher Trask'
   t.console       = true
   t.release       = false
