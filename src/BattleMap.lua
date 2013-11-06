00757dcba4b15998f2ab3fe0d8a4fb6ecd336a11
stefanlevy <stefanlevy@tamu.edu>
2013-11-06 00:03:56 -0600

    created a demo battle map

--- src/BattleMap.lua
+++ src/BattleMap.lua
@@ -20,8 +20,37 @@ function BattleMap.init()
     for x = 0, self.width, 1 do
         for y = 0, self.height, 1 do
             BattleMap.graph:createNode( tiles[x][y] )
-            tiles[x][y].connections[ 0 ] =((math.random() <= .5) and (x<self.width-1))    
-            tiles[x][y].connections[ 3 ] =((math.random() <= .5) and (y<self.height-1))
+			tiles[x][y].connections[ 0 ] =((math.random() <= .3) and (x<self.width-1))    
+			tiles[x][y].connections[ 3 ] =((math.random() <= .3) and (y<self.height))
+			if (x < 3 or x>(self.width - 4))then
+				tiles[x][y].connections[ 0 ] =(x<self.width-1)    
+				tiles[x][y].connections[ 3 ] =(y<self.height)
+			end
+			if (x%5==0)then
+				tiles[x][y].connections[ 3 ] =(y<self.height)
+				if (x%5==0)then
+					tiles[x][y].connections[ 0 ] =(x<self.width-1)  
+				end
+			end
+			if (y<5 and x<20) then
+				tiles[x][y].connections[ 0 ] =(x<self.width) 
+				if (((x%3==0) and (y==2))or((x%2==0) and (y==1))or((x%4==0) and (y==0))) then
+					tiles[x][y].connections[ 3 ] =(y<self.height)
+				end
+			end
+			if (y>10 and x>10) then
+				tiles[x][y].connections[ 0 ] =(x<self.width) 
+				if (((x%3==0) and (y==12))or((x%2==0) and (y==13))or((x%4==0) and (y==14))) then
+					tiles[x][y].connections[ 3 ] =(y<self.height)
+				end
+			end
+			if (y<5 and x>19 and x<23) then
+				tiles[x][y].connections[ 0 ] = 0    
+				tiles[x][y].connections[ 3 ] = 0
+			end
+			if((y==6 and ((x>=10 and x<15)or (x>=25 and  x<30))) or (y==8 and (x>=5 and x<10)) or (y==9 and (x>=20 and x<25)) )then 
+				tiles[x][y].connections[ 0 ] =(x<self.width) 
+			end
             if x > 0 then
                 tiles[x][y].connections[2] = tiles[x-1][y].connections[0]
             end
